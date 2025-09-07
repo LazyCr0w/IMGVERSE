9ddcc0e96cbfd7f002af2fc90ee56ae9c075920a
LazyCr0w <sdfrosty777@gmail.com>
2025-09-07 23:36:38 +0530

    Render deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -77,4 +77,5 @@ app.get("/api/download", async (req, res) => {
   }
 });
 
-app.listen(3001, () => console.log("Server running on http://localhost:3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
